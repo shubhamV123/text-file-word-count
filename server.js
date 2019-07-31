cc5a307d415cc94730b91c42d49c971bbec2519a
Shubham <sv.code97@gmail.com>
2019-08-01 01:59:52 +0530

    testing

--- server.js
+++ server.js
@@ -14,7 +14,8 @@ app.use(cors());
 const root = require('path').join(__dirname, 'client', 'build')
 app.use(express.static(root));
 app.get("*", (req, res) => {
-    res.sendFile('index.html', { root });
+    const index = path.join(__dirname, 'build', 'index.html');
+    res.sendFile(index);
 })
 
 
